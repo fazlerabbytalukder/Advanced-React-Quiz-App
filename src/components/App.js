72a0b3b8e3d16e747e33cd7994d72064c55bb530
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-09-24 20:54:49 -0700

    router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import '../styles/App.css';
 import Layout from './Layout';
 import Result from './pages/Result';
-// import Home from './pages/Home';
-// import Login from './pages/Login';
-// import Quiz from './pages/Quiz';
-// import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Quiz from './pages/Quiz';
+import Signup from './pages/Signup';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        {/* <Home />
-        <Signup />
-        <Login /> */}
-        {/* <Quiz/> */}
-        <Result/>
-      </Layout>
-      
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/quiz" component={Quiz} />
+            <Route exact path="/result" component={Result} />
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
